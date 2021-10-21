5c984fed9b8c3f2a685d6eb4051fa71c26c96b1e
Sebastian Rasmussen <sebras@gmail.com>
2021-10-21 23:15:11 +0200

    Add extraction of font for Tandberg TDV 2200/9 S.

--- README.md
+++ README.md
@@ -24,3 +24,21 @@ of the font.
 This is the output to expect, albeit converted to PNG:
 
 ![Image of font for Norsk Data VDU 301 S](https://github.com/sebras/norsk-data-vdu-301-s/blob/master/font-nd-vdu-301-s.png)
+
+The liberty was also taken to include the analysis of the Tandberg TDV 2200/9 S t
+ROM image also [provided by Torfinn](http://heim.bitraf.no/tingo/td/tandberg-tdv-2200-9s.html).
+
+The font can similarly be extracted like so:
+
+> python3 extract-tdv.py
+
+You should get a `font-tdv-2200-9-s.ppm` based on the original ROM image
+which contains 1024 character concatenated without intervening padding.
+Each character is 8 pixels wide and 16 rows tall, for a total of 16 bytes.
+Like for ND VDU 301 S above, a bit set to `1` should be interpreted as
+black color while a bit cleared to `0` should be interpreted as white
+color.
+
+This is the output to expect for TDV 2200/9 S, albeit converted to PNG:
+
+![Image of font for Tandberg TDV 2200/9 S](https://github.com/sebras/norsk-data-vdu-301-s/blob/master/font-tdv-2200-9-s.png)
---
+++ extract-tdv.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+
+print('Read complete ROM image.')
+with open('tdv2200s_U63_D27128A_965115_-1-.bin', 'rb') as f:
+    font = f.read()
+
+
+print('Blit each character in font into an image.')
+charwidth = 8
+charheight = 16
+padding = 2
+charsperline = 16
+chars = 1024
+lines = int((chars + (charsperline - 1)) / charsperline)
+
+width = padding + charsperline * (charwidth + padding)
+height = padding + lines * (charheight + padding)
+
+rgb = []
+for y in range(height):
+    for x in range(width):
+        rgb.append(-1)
+
+cy = padding
+cx = padding
+
+for char in range(chars):
+    start = char * charheight
+    end = start + charheight
+    chardata = font[start:end]
+
+    byte = 0
+    bit = 7
+    for y in range(charheight):
+        for x in range(charwidth):
+            v = (chardata[byte] >> bit) & 1
+            bit = bit - 1
+            if bit < 0:
+                bit = 7
+                byte = byte + 1
+
+            dx = cx + x
+            dy = cy + y
+
+            offset = dy * width + dx
+            rgb[offset] = v
+
+    cx = cx + charwidth + padding
+    if cx >= width:
+        cx = padding
+        cy = cy + charheight + padding
+
+
+print('Output sample image with complete font.')
+with open('font-tdv-2200-9-s.ppm', 'w') as output:
+    output.write('P3\n')
+    output.write('%u %u\n255\n' % (width, height))
+
+    for y in range(height):
+        for x in range(width):
+            if rgb[y * width + x] == 0:
+                output.write('255 255 255 ')
+            elif rgb[y * width + x] == 1:
+                output.write('  0   0   0 ')
+            else:
+                output.write('127 127 127 ')
